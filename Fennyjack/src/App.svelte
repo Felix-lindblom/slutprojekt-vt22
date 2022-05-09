42da5c41fcf324d6b4379bb83a22ab6d4c1fdc7a
Felix Lindblom <felix.lindblom@elev.ga.ntig.se>
2022-05-09 17:02:07 +0200

    0.1.0 the start

---
+++ Fennyjack/src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	
+</script>
+
+<main>
+	
+	<body class="wrapper">
+		<header>
+			<img src="../img/fennyJack.png" alt="Fenny jack logo">
+			<aside class="VLcon">
+				<h2>VL</h2>
+				<article>
+					<!-- poäng in här -->
+				</article>
+				<aside><h3>I</h3></aside>
+				<!-- För att förklara vad vl innebär och vad det står för. Vinst och förlust förhållande -->
+			</aside>
+		</header>
+		<main class="pokerTabel">
+			<article class="houseArea">
+				<!-- huset kort här -->
+			</article>
+			<aside id="husCounter">
+				<!-- <h3></h3> -->
+			</aside>
+			<article class="playerArea">
+				<aside id="playerCounter">
+
+				</aside>
+				<section id="playerCards">
+
+				</section>
+			</article>
+		</main>
+	</body>
+</main>
+
+<style lang="scss">
+	@mixin center{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	header{
+		display: flex;
+	}
+</style>
