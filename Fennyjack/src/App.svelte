36b002891efeec4e1ac8c6b15f917ed5ef2146b4
Felix Lindblom <74712636+Felix-lindblom@users.noreply.github.com>
2022-05-15 22:35:07 +0200

    0.2.0 whosh and push
    lite scss är klar

--- Fennyjack/src/App.svelte
+++ Fennyjack/src/App.svelte
@@ -10,6 +10,7 @@
 			<aside class="VLcon">
 				<h2>VL</h2>
 				<article>
+					<h2>2.0</h2>
 					<!-- poäng in här -->
 				</article>
 				<aside><h3>I</h3></aside>
@@ -18,6 +19,13 @@
 		</header>
 		<main class="pokerTabel">
 			<article class="houseArea">
+				<figure>
+					<img src="../img/fennyJack.png" alt="Fenny jack logo">
+					<h1>J</h1>
+					<p><em>10</em></p>
+				</figure>
+				
+				
 				<!-- huset kort här -->
 			</article>
 			<aside id="husCounter">
@@ -25,7 +33,7 @@
 			</aside>
 			<article class="playerArea">
 				<aside id="playerCounter">
-
+					
 				</aside>
 				<section id="playerCards">
 
@@ -41,8 +49,68 @@
 		justify-content: center;
 		align-items: center;
 	}
+	@mixin sidesPad($num){
+		padding-left: $num;
+		padding-right: $num;
+	}
 
+	$headercol: rgb(230, 139, 20) ;
+	$vlcol:rgb(100, 25, 5) ;
+	$Whittext:rgb(250,250,250);
+	$cardback:rgb(255, 250, 235);
+	$pokergreen:rgb(53,101,77);
+	$greybak:rgb(75, 75, 75);
+	
 	header{
+		height: 10vh;
+		background-color: $headercol;
 		display: flex;
+		justify-content: space-between;
+		img{
+			@include sidesPad(1vw);
+		}
+		.VLcon{
+			@include center() ;
+			@include sidesPad(3vw);
+			width: 10vw;
+			background-color: $vlcol;
+			color: $Whittext;
+			justify-content: space-between;
+			article{
+				background-color: $greybak;
+				@include sidesPad(1vw)
+			
+			}
+			aside{
+
+			}
+		}
+	}
+
+	.pokerTabel{
+		height: 90vh;
+		background-color: $pokergreen;
+
+		.houseArea{
+			@include center();
+			
+			figure{
+				background-color: $cardback;
+				height: 30rem;
+				width: 20rem;
+				margin: 2rem;
+				@include center();
+				justify-content: space-around;
+				flex-direction: column;
+				border-radius: 1rem;
+				img{
+					height: 7rem;
+					width: auto;
+				}
+				h1{
+					font-size: 4rem;
+				}
+			}
+		}
 	}
 </style>
