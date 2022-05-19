09dbd61629050fd9028abf6733b05f250112b5d6
Felix Lindblom <74712636+Felix-lindblom@users.noreply.github.com>
2022-05-19 22:47:56 +0200

    0.5.0 visar api värden och converterar till blackjack värden
    rätt så bra

--- Fennyjack/src/App.svelte
+++ Fennyjack/src/App.svelte
@@ -4,34 +4,54 @@
 https://deckofcardsapi.com/ 
 */
 
-async function getData(){
+	async function getData(){
 
+		
+		const deckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 
-	const deckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+		let resp = await fetch(deckUrl);
+		let deckID = await resp.json();
+		console.log('Detta är id av din kortlek "' + deckID.deck_id +'"');
+		/* console.log(deckID) */
+		/* bara testa att dra kort */
+		
 
-	let resp = await fetch(deckUrl);
-	let deckID = await resp.json();
-	console.log('Detta är id av din kortlek "' + deckID.deck_id +'"');
-	console.log(deckID)
-	/* bara testa att dra kort */
-	
+		/* körs 2 gånger nu, men det är inte en problem med att dealer ändå ska ha 2 dirket. eller vänta dealer ska få sist */
+		
+			let cardDrawn = 'https://deckofcardsapi.com/api/deck/'+deckID.deck_id+'/draw/?count=2'
+			let Sresp = await fetch(cardDrawn);
+			let card = await Sresp.json();
+			console.log(card.cards)
+			
+			console.log(card.cards[0].value)
+			console.log(card.cards[1].value)
+			console.log('taken cards'+ card.cards.length)
+			
+			
+		for (let i=0;i<card.cards.length;i++){
+			if(isNaN(card.cards[i].value)===true){
+				if (card.cards[i].value === "ACE"){
+					card.cards[i].bjValue = 11
+				}else{
+					card.cards[i].bjValue = 10
+				}
+			}else{
+				card.cards[i].bjValue = card.cards[i].value
+			}
+			
+		}	
 
-	/* körs 2 gånger nu */
-	let cardDrawn = 'https://deckofcardsapi.com/api/deck/'+deckID.deck_id+'/draw/?count=2'
-	let Sresp = await fetch(cardDrawn);
-	let card = await Sresp.json();
-	console.log(card.cards[0])
-	console.log(card.cards[0].value)
-	let cardLetter = card.cards[0].value;
-}
+		console.log(card)
+		
+		return card
+		
+	}
 
 
 </script>
 
 <main>
-{#await getData()} 
 
-{/await}
 	<body class="wrapper">
 		<header>
 			<img src="../img/fennyJack.png" alt="Fenny jack logo">
@@ -47,42 +67,58 @@ async function getData(){
 		</header>
 		<main class="pokerTabel">
 			<article class="houseArea">
-				<figure>
-					<img src="../img/fennyJack.png" alt="Fenny jack logo">
-					<h1>j</h1>
-					<p><em>10</em></p>
-				</figure>
-				<figure>
-					<img src="../img/fennyJack.png" alt="Fenny jack logo">
-					<h1>9</h1>
-					<p><em>9</em></p>
-				</figure>
+
+
+				{#await getData()} 
+					<h1 class="loading">wait for dealers card</h1>
+					{:then card}
+						{#each card.cards as playcard,i}
+							
+						<figure>
+							<img src="../img/fennyJack.png" alt="Fenny jack logo">
+							<h1>{card.cards[i].value}</h1>
+							<p><em>
+								
+								{card.cards[i].bjValue}
+							
+							</em></p>
+						</figure> 
+
+						{/each}
+						
+					
+				{/await}	
+
 				
 				<!-- huset kort här -->
 			</article>
 			<aside id="husCounter">
-				<!-- <h3></h3> -->
+				<h3>20</h3>
 			</aside>
 			<article class="playerArea">
 				<aside id="playerCounter">
-					
+					<h3>19</h3>
 				</aside>
 				<section id="playerCards">
-					<figure>
-						<img src="../img/fennyJack.png" alt="Fenny jack logo">
-						<h1>5</h1>
-						<p><em>5</em></p>
-					</figure>
-					<figure>
-						<img src="../img/fennyJack.png" alt="Fenny jack logo">
-						<h1>Q</h1>
-						<p><em>10</em></p>
-					</figure>
-					<figure>
-						<img src="../img/fennyJack.png" alt="Fenny jack logo">
-						<h1>6</h1>
-						<p><em>6</em></p>
-					</figure>
+					{#await getData()} 
+						<h1 class="loading">wait for players card</h1>
+						{:then card}
+							{#each card.cards as playcard,i}
+								
+							<figure>
+								<img src="../img/fennyJack.png" alt="Fenny jack logo">
+								<h1>{card.cards[i].value}</h1>
+								<p><em>
+									
+									{card.cards[i].bjValue}
+								
+								</em></p>
+							</figure> 
+
+							{/each}
+							
+						
+					{/await}	
 				</section>
 			</article>
 		</main>
@@ -111,7 +147,11 @@ async function getData(){
 	$cardback:rgb(255, 250, 235);
 	$pokergreen:rgb(53,101,77);
 	$greybak:rgb(75, 75, 75);
+	$greytext:rgb(170, 150, 150);
+	$gold:rgb(255, 215, 0);
+	$browntext:rgb(54, 22, 13) ;
 	
+
 	header{
 		height: 10vw;
 		background-color: $headercol;
@@ -138,7 +178,10 @@ async function getData(){
 			}
 		}
 	}
-
+	.loading{
+		font-size: 7rem;
+		color: $Whittext;
+	}
 	.pokerTabel{
 		height: 90vh;
 		background-color: $pokergreen;
@@ -146,28 +189,57 @@ async function getData(){
 
 		.houseArea{
 			@include center();
+
+		}
+		#husCounter{
+			@include center();
+			background-color: $gold;
+			color: $browntext;
+			h3{
+				font-size: 2rem;
+			}
 		}
 
-		#playerCards{
-			@include center();
-		}		
-	}
+		.playerArea{
+
+			#playerCounter{
+				height: 2*4rem;
+				width: 2*4rem;
+				background-color: $greybak;
+				@include center();
+				padding-left: 20vw;
+				h3{
+					font-size: 4rem
+				}
+			}
+
+			#playerCards{
+				@include center();
+			}		
+		}
+}
 
 	/* Vill nog byta det till en klass */
+	/* fundera om du istället vill ha vissa element placeras unkit såsom om  fennyjack placeras relativt till top och value i mitten */
 	figure{
 		background-color: $cardback;
 		@include cardRatio(10,rem );
-		margin: 2rem;
+		
 		@include center();
-		justify-content: space-around;
+		justify-content: space-between;
 		flex-direction: column;
-		border-radius: 1rem;
+		border-radius: 3ex;
 		img{
-			height: 7rem;
+			height: 25%;
 			width: auto;
 		}
 		h1{
-			font-size: 4rem;
+			font-size: 600%;
+			
+		}
+		em{
+			color: $greytext;
+			font-size: 300%;
 		}
 	}
 
